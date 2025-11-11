7f7b237fcb336526317aa548e8d89622d9d0f6a4
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2025-11-11 22:46:44 +0700

    add login and signup page

---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignupForm } from '@/presentation/components/auth/SignupForm';
+
+export default function SignupPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <SignupForm />
+    </div>
+  );
+}
